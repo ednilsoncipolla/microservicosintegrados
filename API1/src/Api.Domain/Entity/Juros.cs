e5c5d88b411670902ecfebac9a72a570d6c36612
Ednilson Luciano Cipolla <ednilsoncipolla@marcon.ind.br>
2019-10-07 13:08:44 -0300

    Implementada Entidade Juros

---
+++ API1/src/Api.Domain/Entity/Juros.cs
@@ -0,0 +1,12 @@
+namespace Api.Domain.Entity
+{
+    public class Correcao
+    {
+        private decimal idcCorrecao;
+        public decimal IdcCorrecao
+        {
+            get { return idcCorrecao; }
+            set { idcCorrecao = value; }
+        }
+    }
+}
