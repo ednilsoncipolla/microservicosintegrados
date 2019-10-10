7858de39092ac97b60acecb29b43778d2367db28
Ednilson Luciano Cipolla <ednilsoncipolla@marcon.ind.br>
2019-10-10 13:55:48 -0300

    Inicada a Implementação das Interfaces e Services

---
+++ API1/src/Api.Domain/Entity/Correcao.cs
@@ -0,0 +1,12 @@
+namespace Api.Domain.Entity
+{
+    public class Correcao
+    {
+        private decimal _Taxa;
+        public decimal Taxa
+        {
+            get { return _Taxa; }
+            set { _Taxa = value; }
+        }
+    }
+}
---
+++ API1/src/Api.Domain/Interface/ICorrecaoService.cs
@@ -0,0 +1,10 @@
+using System.Threading.Tasks;
+using Api.Domain.Entity;
+
+namespace Api.Domain.Interface
+{
+    public interface ICorrecaoService
+    {
+         Task<Correcao> GetTaxa(Correcao correcao);
+    }
+}
