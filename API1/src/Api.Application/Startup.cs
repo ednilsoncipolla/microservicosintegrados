b8da425ca54c8045b7b909c679bbaa1ecbeb3854
Ednilson Luciano Cipolla <ednilsoncipolla@marcon.ind.br>
2019-10-15 13:46:40 -0300

    Aplicando Swegger

--- API1/src/Api.Application/Startup.cs
+++ API1/src/Api.Application/Startup.cs
@@ -6,10 +6,12 @@ using Api.CrossCutting.DependencyInjetion;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Application
 {
@@ -26,6 +28,19 @@ namespace Application
         public void ConfigureServices(IServiceCollection services)
         {
             ConfigureService.ConfigureDependencyServices(services);
+
+            services.AddSwaggerGen (c => {
+                c.SwaggerDoc ("v1",
+                    new Info {
+                        Title = "ApiNetCore 2.2 taxaJuros",
+                        Version = "v1",
+                        Description = "Exemplo de api que fornece a taxa de atualização de valores",
+                        Contact = new Contact {
+                            Name = "Ednilson Luciano Cipolla",
+                            Url = "https://github.com/ednilsoncipolla"
+                        }
+                    });
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -36,6 +51,17 @@ namespace Application
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI (c => {
+                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint ("/swagger/v1/swagger.json",
+                "Projeto em .Net Core 2.2");
+            });
+
+            var option = new RewriteOptions ();
+            option.AddRedirect("^$","swagger");
+            app.UseRewriter(option);
 
             app.UseMvc();
         }
